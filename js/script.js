fa01377b0f1dd1a54574bf1de466899c1277a0c2
Rafael <rafaelbritovaladares@gmail.com>
2025-04-05 12:27:50 -0300

    Melhoria

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@ home.addEventListener("click", () => {
 logoImg.addEventListener("click", () => {
     window.location.href = "index.html";
 });
+
+function sendEmail() {
+    const doubtText = document.getElementById('doubtText').value;
+  
+    if (doubtText) {
+      const mailtoLink = `mailto:rafael.valadares@ucsal.edu.br?subject=Dúvida&body=${encodeURIComponent(doubtText)}`;
+      window.location.href = mailtoLink;
+    } else {
+      alert('Por favor, escreva sua dúvida antes de enviar.');
+    }
+  }
+  
